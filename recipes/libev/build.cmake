4fb6dc96896a025917ca0c78e4842892525c1f85
Paul <pfultz2@yahoo.com>
2017-02-23 15:32:17 -0800

    Escape quotes

--- recipes/libev/build.cmake
+++ recipes/libev/build.cmake
@@ -172,34 +172,34 @@ file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/config.h.in "
 
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
-#define LT_OBJDIR ".libs/"
+#define LT_OBJDIR \".libs/\"
 
 /* Name of package */
-#define PACKAGE "libev"
+#define PACKAGE \"libev\"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT ""
+#define PACKAGE_BUGREPORT \"\"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME ""
+#define PACKAGE_NAME \"\"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING ""
+#define PACKAGE_STRING \"\"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME ""
+#define PACKAGE_TARNAME \"\"
 
 /* Define to the home page for this package. */
-#define PACKAGE_URL ""
+#define PACKAGE_URL \"\"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION ""
+#define PACKAGE_VERSION \"\"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#cmakedefine VERSION "@DIST_VERSION@"
+#cmakedefine VERSION \"@DIST_VERSION@\"
 
 ")
 configure_file ( ${CMAKE_CURRENT_BINARY_DIR}/config.h.in ${CMAKE_CURRENT_BINARY_DIR}/config.h )
