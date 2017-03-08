382a8e76474c85a7ba7ac551c0e6012b1d6874c7
Paul <pfultz2@yahoo.com>
2017-03-07 18:55:19 -0600

    Try using a zip file instead

--- recipes/boost/build.cmake
+++ recipes/boost/build.cmake
@@ -1,5 +1,12 @@
 cmake_minimum_required(VERSION 2.8)
 
+
+if(UNIX)
+    # Make sure its executable
+    execute_process(COMMAND chmod +x ${CMAKE_CURRENT_SOURCE_DIR}/tools/build/bootstrap.sh)
+    execute_process(COMMAND chmod +x ${CMAKE_CURRENT_SOURCE_DIR}/tools/build/src/engine/build.sh)
+endif()
+
 # See:
 # https://github.com/boostorg/asio/pull/45
 # https://github.com/chriskohlhoff/asio/issues/85
