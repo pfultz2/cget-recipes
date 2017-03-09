d83c777962f34637d209ae3e3cc51662ffbc5af2
Paul <pfultz2@yahoo.com>
2017-03-09 08:21:46 -0600

    Update to use cmake utilities

--- recipes/cginternals/glbinding/build.cmake
+++ recipes/cginternals/glbinding/build.cmake
@@ -1,14 +1,6 @@
 cmake_minimum_required(VERSION 2.8)
 
-function(patch_file FILE MATCH REPLACEMENT)
-    message(STATUS "Patch: ${FILE}")
-    file(READ ${FILE} CONTENT)
-    string(REPLACE 
-        "${MATCH}"
-        "${REPLACEMENT}" 
-        OUTPUT_CONTENT "${CONTENT}")
-    file(WRITE ${FILE} "${OUTPUT_CONTENT}")
-endfunction()
+find_package(cget-recipe-utils)
 
 # MSVC 2015 crashes when adding the /GL flag
 # See https://gitlab.kitware.com/cmake/cmake/issues/16282
