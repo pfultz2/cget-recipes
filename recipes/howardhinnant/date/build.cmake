40ebda53f576a2a8a5e85fb88c564facd405de25
Paul <pfultz2@yahoo.com>
2017-09-28 11:32:51 -0500

    Update howardhinnant/date recipe

--- recipes/howardhinnant/date/build.cmake
+++ recipes/howardhinnant/date/build.cmake
@@ -3,11 +3,10 @@ if(NOT MSVC)
     add_definitions(-std=c++0x)
 endif()
 
-include_directories(${CMAKE_CURRENT_SOURCE_DIR})
-add_library(tz tz.cpp)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/include)
+add_library(tz src/tz.cpp)
 install(TARGETS tz DESTINATION lib)
 
-file(GLOB HEADERS *.h)
-install(FILES ${HEADERS} DESTINATION include/date)
+install(DIRECTORY include/ DESTINATION include)
 
 enable_testing()
